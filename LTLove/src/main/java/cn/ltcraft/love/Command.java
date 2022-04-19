fcd82e230bef19271f171555a89e4cf1940d1896
smallXueTu <2665337794@qq.com>
2022-04-19 23:32:03 +0800

    进一步完善结婚

--- LTLove/src/main/java/cn/ltcraft/love/Command.java
+++ LTLove/src/main/java/cn/ltcraft/love/Command.java
@@ -6,11 +6,16 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author 寻琴
  * @year 2022年04月19日20:35
  */
 public class Command implements CommandExecutor {
+    //请求
+    private Map<String, String> request = new HashMap<>();
     /**
      * 命令一定是/结婚
      * @author 寻琴
@@ -30,20 +35,48 @@ public class Command implements CommandExecutor {
         if (arg1.equals("")){
             arg1 = args[0];
         }
-        String sex;
+        Love.Sex sex;
+        PlayerConfig playerConfig;
+        PlayerConfig targetConfig;
+        Player player = null;
+        if (sender instanceof Player)player = ((Player) sender);
         switch (arg1){
             case "结婚":
+                if (!(sender instanceof Player)){
+                    sender.sendMessage("§c你不是一个玩家！");
+                    return true;
+                }
                 if (args.length < 2){
                     sender.sendMessage("§e输入§d/结婚 求婚 目标ID§e来向一个玩家求婚！");
                     return true;
                 }
+                playerConfig = PlayerConfig.getPlayerConfig(player);
+                sex = Love.Sex.byName(playerConfig.getConfig().getString("性别"));
+                if (sex == Love.Sex.NONE){
+                    sender.sendMessage("§e你还没选择性别，输入§d/结婚 性别 [男/女]§e选择自己的性别!");
+                    return true;
+                }
+                if (sex == Love.Sex.Secrecy){
+                    sender.sendMessage("§e对方性别保密，你不能跟ta结婚！");
+                    return true;
+                }
                 String targetName = args[1];
-                Player player = Bukkit.getPlayer(targetName);
-                if (player == null || !player.isOnline()){
+                Player target = Bukkit.getPlayer(targetName);
+                if (target == null || !target.isOnline()){
                     sender.sendMessage("§e玩家" + targetName + "不在线！");
                     return true;
                 }
-                PlayerConfig playerConfig = PlayerConfig.getPlayerConfig(player);
+                targetConfig = PlayerConfig.getPlayerConfig(player);
+                Love.Sex targetSex = Love.Sex.byName(targetConfig.getConfig().getString("性别"));
+                if (targetSex == Love.Sex.NONE){
+                    sender.sendMessage("§e你还不知道对方性别，你敢结婚吗！");
+                    return true;
+                }
+                String love = targetConfig.getConfig().getString("伴侣");
+                if (love != null && !love.isEmpty()){
+                    sender.sendMessage("§e哦 太悲催了，你的" + targetSex.getName() + "神已经结婚了！");
+                    return true;
+                }
                 break;
             case "帮助":
                 sender.sendMessage("§e输入§d/结婚 求婚 目标ID§e来向一个玩家求婚！");
--- LTLove/src/main/java/cn/ltcraft/love/Love.java
+++ LTLove/src/main/java/cn/ltcraft/love/Love.java
@@ -1,7 +1,12 @@
 package cn.ltcraft.love;
 
+import cn.LTCraft.core.task.PlayerClass;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author 寻琴
@@ -13,5 +18,35 @@ public class Love extends JavaPlugin {
         super.onEnable();
         Bukkit.getPluginManager().registerEvents(new Listener(), this);
         getCommand("结婚").setExecutor(new Command());
+    }
+    public static enum Sex{
+        NONE(""),
+        Male("男"),
+        Female("女"),
+        Secrecy("保密");
+        private static final Map<String, Sex> BY_NAME = new HashMap();
+        private String name;
+        Sex(String name){
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
+        /**
+         * 通过名字查找性别
+         * @param name 职业性别
+         * @return 性别
+         */
+        public static Sex byName(String name){
+            return BY_NAME.getOrDefault(name, NONE);
+        }
     }
 }
