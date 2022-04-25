a3503cd7bfc083819eb4e1489cfab36cbcafeba7
smallXueTu <2665337794@qq.com>
2022-04-26 00:26:39 +0800

    坐标Util

--- LTCraft/src/main/java/cn/LTCraft/core/utils/WorldUtils.java
+++ LTCraft/src/main/java/cn/LTCraft/core/utils/WorldUtils.java
@@ -1,10 +1,14 @@
 package cn.LTCraft.core.utils;
 
 
+import cn.ltcraft.item.base.ItemTypes;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class WorldUtils {
     /**
@@ -42,13 +46,65 @@ public class WorldUtils {
         return location.add(x, 0, z);
     }
 
-    public static void main(String[] args) {
-        int xx = 0;
-        int zz = 0;
-        for (int i = 0; i < 100; i++) {
-            double x = (Utils.getRandom().nextInt((int) (5 * 200)) - 5d * 100) / 100;
-            double z = (Utils.getRandom().nextInt((int) (5 * 200)) - 5d * 100) / 100;
-            System.out.println("x:" + (xx + x) + ", z:" + (zz + z));
+    /**
+     * 获取制定面的坐标
+     * @param location 坐标
+     * @param side 面
+     * @return 坐标
+     */
+    public static Location getSide(Location location, SIDE side){
+        return getSide(location, side, 1);
+    }
+
+    /**
+     * @param side 步长
+     */
+    public static Location getSide(Location location, SIDE side, int step){
+        switch(side){
+            case DOWN://下
+                return location.add(0, - step, 0);
+            case UP://上
+                return location.add(0, step, 0);
+            case NORTH:
+                return location.add(0, 0, - step);
+            case SOUTH:
+                return location.add(0, 0, step);
+            case WEST:
+                return location.add(- step, 0, 0);
+            case EAST:
+                return location.add(step, 0, 0);
+            default:
+                return location;
+        }
+    }
+
+    /**
+     * 方块面
+     */
+    enum SIDE{
+        DOWN(0),//下 y - 1
+        UP(1),//上 y + 1
+        NORTH(2),//北 z - 1
+        SOUTH(3),//男 z + 1
+        WEST(4),//西 x - 1
+        EAST(5);//东 x + 1
+        private static final Map<Integer, SIDE> BY_NAME = new HashMap<>();
+        static {
+            SIDE[] classes = values();
+            for (SIDE aClass : classes) {
+                BY_NAME.put(aClass.getId(), aClass);
+            }
+        }
+        private final int id;
+        SIDE(int id){
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+        public static SIDE byName(Integer id){
+            return BY_NAME.get(id);
         }
     }
 }
