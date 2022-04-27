7edc7f1dbd8546061804b1275fbb9821286cb4d8
smallXueTu <2665337794@qq.com>
2022-04-27 21:05:12 +0800

    获取大写数字函数

--- LTCraft/src/main/java/cn/LTCraft/core/utils/Utils.java
+++ LTCraft/src/main/java/cn/LTCraft/core/utils/Utils.java
@@ -202,6 +202,48 @@ public class Utils {
         }
         return str;
     }
+    /**
+     * 获取大写数字
+     * @return 大写数字
+     */
+    public static String getNumberCapitalize(int level) {
+        String str = "";
+        switch (level){
+            case 1:
+                str = "一";
+                break;
+            case 2:
+                str = "二";
+                break;
+            case 3:
+                str = "三";
+                break;
+            case 4:
+                str = "四";
+                break;
+            case 5:
+                str = "五";
+                break;
+            case 6:
+                str = "六";
+                break;
+            case 7:
+                str = "七";
+                break;
+            case 8:
+                str = "八";
+                break;
+            case 9:
+                str = "九";
+                break;
+            case 0:
+                str = "零";
+                break;
+            default:
+                str = String.valueOf(level);
+        }
+        return str;
+    }
 
     /**
      * 格式化数字
