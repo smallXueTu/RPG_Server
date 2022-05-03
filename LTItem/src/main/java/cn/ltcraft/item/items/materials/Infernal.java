cf1a945f54348bf3e7913bc8c3c3f789ed69c478
smallXueTu <2665337794@qq.com>
2022-05-04 00:31:50 +0800

    只有拿在手上才有效果

--- LTItem/src/main/java/cn/ltcraft/item/items/materials/Infernal.java
+++ LTItem/src/main/java/cn/ltcraft/item/items/materials/Infernal.java
@@ -5,9 +5,12 @@ import cn.ltcraft.item.items.Material;
 import net.minecraft.server.v1_12_R1.DamageSource;
 import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftInventoryPlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
 
 /**
+ * 火魔
  * Created by Angel、 on 2022/5/3 21:28
  */
 public class Infernal extends Material implements TickItem {
@@ -17,6 +20,9 @@ public class Infernal extends Material implements TickItem {
 
     @Override
     public void doTick(long tick, Player player, int invIndex) {
-        ((CraftPlayer)player).getHandle().damageEntity(DamageSource.BURN, 2);
+        PlayerInventory inventory = player.getInventory();
+        if (inventory.getHeldItemSlot() == invIndex || invIndex == inventory.getSize() - 1) {
+            ((CraftPlayer) player).getHandle().damageEntity(DamageSource.BURN, 2);
+        }
     }
 }
