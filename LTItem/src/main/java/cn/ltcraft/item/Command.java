cfcffa27b518bc288684bc1c3dbd86eaae499e9f
smallXueTu <2665337794@qq.com>
2022-05-06 11:30:29 +0800

    饰品类型

--- LTItem/src/main/java/cn/ltcraft/item/Command.java
+++ LTItem/src/main/java/cn/ltcraft/item/Command.java
@@ -156,6 +156,7 @@ public class Command implements CommandExecutor {
                                     yamlConfiguration.set("模型", args[3]);
                                     yamlConfiguration.set("名字", args[2]);
                                     yamlConfiguration.set("品质", "未知");
+                                    yamlConfiguration.set("饰品类型", "任意");
                                     yamlConfiguration.set("显示名字", "§6" + args[2]);
                                     yamlConfiguration.set("说明", new ArrayList<>());
                                     pvp = new HashMap<>(PVP);
