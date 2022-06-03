06b6c73e6d1ff364677c0ff84e29deadf8639ce2
smallXueTu <2665337794@qq.com>
2022-06-03 17:50:50 +0800

    品质限制

--- LTItem/src/main/java/cn/ltcraft/item/items/gemsstones/EternalCrystal.java
+++ LTItem/src/main/java/cn/ltcraft/item/items/gemsstones/EternalCrystal.java
@@ -1,14 +1,21 @@
 package cn.ltcraft.item.items.gemsstones;
 
+import cn.ltcraft.item.base.interfaces.ConfigurableLTItem;
+import cn.ltcraft.item.base.interfaces.LTItem;
 import cn.ltcraft.item.items.GemsStone;
+import cn.ltcraft.item.utils.Utils;
 import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Angel、 on 2022/6/2 12:31
  */
 public class EternalCrystal extends GemsStone {
+    private static List<String> List = Arrays.asList("无品质", "未知", "入门", "初级", "普通", "中级");
     public EternalCrystal(MemoryConfiguration configuration) {
         super(configuration);
     }
@@ -17,8 +24,17 @@ public class EternalCrystal extends GemsStone {
     public ItemStack installOn(ItemStack itemStack) {
         if (itemStack.hasItemMeta()) {
             ItemMeta itemMeta = itemStack.getItemMeta();
-            itemMeta.setUnbreakable(true);
-            itemStack.setItemMeta(itemMeta);
+            if (!itemMeta.isUnbreakable()) {
+                LTItem ltItems = Utils.getLTItems(itemStack);
+                if (ltItems instanceof ConfigurableLTItem) {
+                    ConfigurableLTItem configurableLTItem = (ConfigurableLTItem) ltItems;
+                    String string = configurableLTItem.getConfig().getString("品质", "无品质");
+                    if (List.contains(string)) {
+                        itemMeta.setUnbreakable(true);
+                        itemStack.setItemMeta(itemMeta);
+                    }
+                }
+            }
         }
         return itemStack;
     }
