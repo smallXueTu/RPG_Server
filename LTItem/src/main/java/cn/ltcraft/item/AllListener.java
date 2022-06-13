d0bb521ee280e6e7aa5640dae22abc1a3b5f9914
smallXueTu <2665337794@qq.com>
2022-06-13 14:09:34 +0800

    盔甲架不再显示掉血

--- LTItem/src/main/java/cn/ltcraft/item/AllListener.java
+++ LTItem/src/main/java/cn/ltcraft/item/AllListener.java
@@ -275,6 +275,7 @@ public class AllListener implements Listener {
             TriggerAction.onDamage(player, event);
         }
         if (entity instanceof LivingEntity && event.getDamage() >= 1){
+            if (entity.getType() == EntityType.ARMOR_STAND)return;
             new FloatText(entity.getLocation().add(0, entity.getHeight(), 0), "§l§c-" + cn.LTCraft.core.utils.Utils.formatNumber(event.getFinalDamage()), 2 * 20, new Vector(0, 0.03, 0));
         }
     }
