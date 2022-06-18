68e0a95118c9b9070122c86d91ce67a972027056
smallXueTu <2665337794@qq.com>
2022-06-19 02:14:04 +0800

    闪避特效

--- LTCraft/src/main/java/cn/LTCraft/core/game/skills/shields/DodgeShield.java
+++ LTCraft/src/main/java/cn/LTCraft/core/game/skills/shields/DodgeShield.java
@@ -1,9 +1,12 @@
 package cn.LTCraft.core.game.skills.shields;
 
+import cn.LTCraft.core.game.more.FloatText;
 import cn.LTCraft.core.game.skills.BaseSkill;
+import cn.LTCraft.core.utils.EntityUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.util.Vector;
 
 
 /**
@@ -23,6 +26,7 @@ public class DodgeShield extends BaseShield{
     @Override
     public boolean block(EntityDamageEvent event) {
         if (event instanceof EntityDamageByEntityEvent){
+            EntityUtils.missAttack(owner);
             event.setCancelled(true);
         }
         return true;
