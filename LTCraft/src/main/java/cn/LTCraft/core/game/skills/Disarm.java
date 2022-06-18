d47a15cd05e63249f88923bded7978a11456713f
smallXueTu <2665337794@qq.com>
2022-06-19 01:28:29 +0800

    完善法师技能

--- LTCraft/src/main/java/cn/LTCraft/core/game/skills/Disarm.java
+++ LTCraft/src/main/java/cn/LTCraft/core/game/skills/Disarm.java
@@ -1,7 +1,9 @@
 package cn.LTCraft.core.game.skills;
 
+import cn.LTCraft.core.Main;
 import cn.LTCraft.core.other.Temp;
 import cn.LTCraft.core.utils.PlayerUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -21,13 +23,23 @@ public class Disarm extends BaseSkill{
         List<Entity> collection = owner.getNearbyEntities(radius, radius, radius);
         for (Entity entity : collection) {
             if (entity instanceof LivingEntity){
-                if (entity instanceof Player){
+                if (entity instanceof Player && entity != owner){
                     ((Player)entity).sendTitle("§l§a受技能影响", "§l§e来自§a"+owner.getName()+"§e的§d缴械§e和§d沉默§e技能，你的主武器已被缴械到背包！");
                     PlayerUtils.castMMSkill(((Player) entity), "缴械自己");
                 }
                 Temp.addSilence(entity, (5 + level) * 20);
             }
         }
-        return false;
+        if (isAwaken()) {
+            if (!owner.getAllowFlight()) {
+                owner.setAllowFlight(true);
+                owner.setFlying(true);
+                Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
+                    owner.setAllowFlight(false);
+                    owner.setFlying(false);
+                }, (3 + awakenLevel) * 20L);
+            }
+        }
+        return true;
     }
 }
