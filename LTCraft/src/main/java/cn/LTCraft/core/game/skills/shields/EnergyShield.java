f2b8c809a77c9301bd13232262688142a07b68fa
smallXueTu <2665337794@qq.com>
2022-06-19 12:58:34 +0800

    战士护盾爆炸

--- LTCraft/src/main/java/cn/LTCraft/core/game/skills/shields/EnergyShield.java
+++ LTCraft/src/main/java/cn/LTCraft/core/game/skills/shields/EnergyShield.java
@@ -1,11 +1,15 @@
 package cn.LTCraft.core.game.skills.shields;
 
+import cn.LTCraft.core.Main;
+import cn.LTCraft.core.game.Game;
 import cn.LTCraft.core.game.skills.BaseSkill;
 import cn.LTCraft.core.other.Temp;
 import cn.LTCraft.core.utils.EntityUtils;
 import cn.LTCraft.core.utils.PlayerUtils;
 import net.minecraft.server.v1_12_R1.DamageSource;
+import org.bukkit.Bukkit;
 import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftEntity;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
@@ -30,11 +34,11 @@ public class EnergyShield extends BaseShield{
             blockDamage += event.getDamage();
             double percentage;
             if (((EntityDamageByEntityEvent) event).getDamager() instanceof Player) {
-                percentage = 30 + level * 10;
-                event.setDamage(event.getDamage() * (percentage / 100 + 1));
+                percentage = 30 + level * 10;//免伤百分比
+                event.setDamage(event.getDamage() * (1 - percentage / 100));
             }else {
                 percentage = 50 + level * 10;
-                event.setDamage(event.getDamage() * (percentage / 100 + 1));
+                event.setDamage(event.getDamage() * (1 - percentage / 100));
             }
             owner.getWorld().playSound(owner.getLocation(), Sound.BLOCK_ANVIL_FALL, 1, 1);
         }
@@ -54,13 +58,19 @@ public class EnergyShield extends BaseShield{
     public void destroy() {
         super.destroy();
         if (isAwaken()){
-            ((CraftWorld) owner.getWorld()).getHandle().createExplosion((((CraftPlayer) owner)).getHandle(), owner.getLocation().getX(), owner.getLocation().getY(), owner.getLocation().getZ(), 5, false, false);
-            Collection<Entity> nearbyEntities = owner.getWorld().getNearbyEntities(owner.getLocation(), 3, 3, 3);
-            double damage = blockDamage * (0.2 + awakenLevel * 0.1);
-            for (Entity nearbyEntity : nearbyEntities) {
-                EntityUtils.repel(nearbyEntity, owner.getLocation(), 2, 10);
-                ((CraftEntity) nearbyEntity).getHandle().damageEntity(DamageSource.a(((CraftPlayer) owner).getHandle()), (float) damage);
-            }
+            Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
+                World world = owner.getWorld();
+                ((CraftWorld) world).getHandle().createExplosion((((CraftPlayer) owner)).getHandle(), owner.getLocation().getX(), owner.getLocation().getY(), owner.getLocation().getZ(), 5, false, false);
+                Collection<Entity> nearbyEntities = world.getNearbyEntities(owner.getLocation(), 3, 3, 3);
+                double damage = blockDamage * (0.2 + awakenLevel * 0.1);
+                for (Entity nearbyEntity : nearbyEntities) {
+                    if (Game.rpgWorlds.contains(world.getName()) && nearbyEntity instanceof Player) {
+                        continue;
+                    }
+                    EntityUtils.repel(nearbyEntity, owner.getLocation(), 2, 10);
+                    ((CraftEntity) nearbyEntity).getHandle().damageEntity(DamageSource.a(((CraftPlayer) owner).getHandle()), (float) damage);
+                }
+            }, 0);
         }
     }
 }
