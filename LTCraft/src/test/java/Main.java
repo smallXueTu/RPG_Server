2b59b14dbd1f4d26244c76f5b1a82402e4c90455
smallXueTu <2665337794@qq.com>
2022-06-30 23:32:37 +0800

    测试代码

--- LTCraft/src/test/java/Main.java
+++ LTCraft/src/test/java/Main.java
@@ -1,9 +1,12 @@
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Angel、 on 2022/6/22 22:14
  */
 public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+        logger.error("${java:vm}");
     }
 }
