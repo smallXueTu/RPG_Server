43f477eacec2b9617e779e17ab17f3909765286a
smallXueTu <2665337794@qq.com>
2022-07-02 23:57:58 +0800

    测试代码

--- LTCraft/src/test/java/Main.java
+++ LTCraft/src/test/java/Main.java
@@ -5,8 +5,7 @@ import org.slf4j.LoggerFactory;
  * Created by Angel、 on 2022/6/22 22:14
  */
 public class Main {
-    private static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        logger.error("${java:vm}");
+        org.bukkit.craftbukkit.Main.main(args);
     }
 }
