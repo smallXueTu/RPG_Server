dc959289ccb9935768ed07e2d2b8b0333cf9a6a2
smallXueTu <2665337794@qq.com>
2022-07-16 16:13:53 +0800

    tryOpenTimer 尝试打开计时器器

--- LTCraft/src/main/java/cn/LTCraft/core/entityClass/spawns/ChestMobSpawn.java
+++ LTCraft/src/main/java/cn/LTCraft/core/entityClass/spawns/ChestMobSpawn.java
@@ -1,10 +1,19 @@
 package cn.LTCraft.core.entityClass.spawns;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * Created by Angel、 on 2022/7/15 23:35
  */
 public class ChestMobSpawn extends AbstractMobSpawn {
+    private final Map<String, Long> tryOpenTimer = new HashMap<>();
     public ChestMobSpawn(String insideName) {
         super(insideName);
+    }
+
+    public Map<String, Long> getTryOpenTimer() {
+        return tryOpenTimer;
     }
 }
